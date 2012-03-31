2ae37e4eb0b7c3c70083f3d113d888abab46f061
Svenn-Arne Dragly <s@dragly.com>
2012-03-31 18:05:59 +0200

    Object oriented the code a lot mroe

--- ex2.js
+++ ex2.js
@@ -21,72 +21,76 @@ function ExSimulator() {
 ExSimulator.prototype = new Simulator();
 ExSimulator.prototype.constructor = ExSimulator;
 
+var l = 300;
+var omega = 1;
+var phi0 = Math.PI/2 - 0.01;
+
 ExSimulator.prototype.animate = function() {
             // clear
             this.ctx.fillStyle = "rgba(255,255,255,0.8)";
             this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
 
-            var m = 1;
+            var m = 4;
             var M = 10;
-            var g = 8000;
-            var l = 100;
-
-            var phi = this.phi;
-            var x = this.x;
-            var velx = this.velx;
-            var velphi = this.velphi;
+            var g = 800;
 
             this.t += this.dt;
 
-            // calculate the forces
-            this.accphi = (-(M+m)*g*Math.sin(phi) - m*velphi*velphi*l*Math.cos(phi)*Math.sin(phi))/(l*(M+m) - l*m*(Math.cos(phi)*Math.cos(phi)));
-//            this.accphi = -(M+m)*g*phi/((M+2*m)*l);
-            this.accx = (m*velphi*velphi*l*Math.sin(phi) - m*this.accphi*l*Math.cos(phi))/(M+m);
-//            this.accx = -m*l*this.accphi/(M+m);
-//            console.log(this.accphi + " " + this.accphi2);
-            this.velx += this.accx * this.dt;
-            this.velphi += this.accphi * this.dt;
-
-            var omega = Math.sqrt(((M+m)*g)/(l*M));
+            var phi = this.phi;
+            var s = this.s;
+            var vels = this.vels;
+            var velphi = this.velphi;
             var t = this.t;
 
-            this.x += this.velx * this.dt;
-            this.x2 = (-l*m*0.1/(M+m))*(Math.cos(omega*t) - Math.cos(0) + Math.sin(0)*(t - 0)) + 0*(t - 0) + 100;
-            var diff = Math.round((this.x - this.x2)*100)/100;
-            this.ctx.fillStyle="rgba(0,0,0,1)";
-            this.ctx.fillText("Diff: " + diff, 10,10);
-            this.phi += this.velphi * this.dt;
+            // calculate the forces
+//            this.accphi = (-2*m*velphi*vels - g*velphi*Math.cos(phi))/s;
+//            this.accphi = -(M+m)*g*phi/((M+2*m)*l);
+            this.accs = velphi*velphi*s - g*Math.sin(phi);
+//            this.accx = -m*l*this.accphi/(M+m);
+//            console.log(this.accphi + " " + this.accphi2);
+            this.vels += this.accs * this.dt;
+//            this.velphi += this.accphi * this.dt;
 
-            if(this.x > this.canvas.width) {
-                this.x = 0;
+            this.s += this.vels * this.dt;
+            var c1 = (l/2 + g/(4*(omega*omega)) * (Math.cos(phi0) - Math.sin(phi0)));
+            var c2 = (l/2 - g/(4*(omega*omega)) * (Math.cos(phi0) + Math.sin(phi0)));
+            var s2 = c1 * Math.exp(omega*t) + c2 * Math.exp(-omega*t) + g/(2*omega*omega)*Math.sin(phi0 - omega*t);
+//            this.s = s2;
+            this.phi += this.velphi * this.dt;
+//            this.phi = phi0 - omega*t;
+
+            if(this.s < 0) {
+                this.s = 0;
+            }
+            if(this.phi < 0) {
+                this.phi = 0;
+                this.velphi = 0;
             }
 
             // drawing positions
-            var cartx = this.x;
-            var carty = 200-150;
+            var cartx = 400-(this.s+50)*Math.cos(phi);
+            var carty = (this.s+50)*Math.sin(phi);
 
-            var ballx = this.x + Math.sin(phi) * l;
-            var bally = 200 - Math.cos(phi) * l;
+            var ballx = 400 - 300*Math.cos(phi);
+            var bally = 300*Math.sin(phi);
+
+            var diff = Math.round((this.s - s2)*100)/100;
+            this.ctx.fillStyle="rgba(0,0,0,1)";
+            this.ctx.fillText("s: " + Math.round(cartx) + "," + Math.round(carty), 10,10);
+            this.ctx.fillText("Diff: " + diff, 10,40);
 
             // draw cart
             this.ctx.fillStyle="rgba(0,200,100,1)";
             this.ctx.strokeStyle="rgba(0,200,100,1)";
-            this.ctx.fillRect(cartx - 50, this.canvas.height - carty - 25, 100, 50);
-            // draw wheels
-            this.ctx.beginPath();
-            this.ctx.arc(cartx - 25, this.canvas.height - (carty - 40), 10, 0, Math.PI*2, true);
-            this.ctx.closePath();
-            this.ctx.fill();
-            this.ctx.beginPath();
-            this.ctx.arc(cartx + 25, this.canvas.height - (carty - 40), 10, 0, Math.PI*2, true);
-            this.ctx.closePath();
-            this.ctx.fill();
+            this.ctx.save();
+            this.ctx.translate(cartx,this.canvas.height - carty);
+            this.ctx.rotate(phi);
+            this.ctx.fillRect(0, -50, 100, 50);
+            this.ctx.restore();
             // draw pole
             this.ctx.beginPath();
-            this.ctx.moveTo(cartx, this.canvas.height - (carty));
-            this.ctx.lineTo(cartx, this.canvas.height - (carty + 150));
+            this.ctx.moveTo(400, this.canvas.height - (0));
             this.ctx.lineTo(ballx, this.canvas.height - (bally));
-//            this.ctx.closePath();
             this.ctx.stroke();
             // draw ball
             this.ctx.beginPath();
@@ -107,9 +111,9 @@ window.onload = function(){
 
             simulator.dt = 0.005;
 
-            simulator.x = 100;
-            simulator.phi = 0.3;
-            simulator.velx = 0;
-            simulator.velphi = 5.0;
+            simulator.s = l;
+            simulator.phi = phi0;
+            simulator.vels = 0;
+            simulator.velphi = -omega;
             simulator.animate();
         };
